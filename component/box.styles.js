5bef82c990a0fcfe244ce53bf8fafe1b9aef150b
oliver-lieu <bright_96_628@outlook.com>
2021-05-14 01:00:04 +0800

    step bar bottom

--- component/box.styles.js
+++ component/box.styles.js
@@ -194,6 +194,7 @@ export const SubStep = styled.div.attrs({
         height: 0.5rem;
         background-color: #ddd;
         margin-top: 2rem;
+        margin-bottom: 1rem;
         border-radius: 0.25rem;
     }
 }
